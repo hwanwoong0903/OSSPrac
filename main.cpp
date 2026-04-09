b847cc89d7cef12146482799fd5c4f8608f4e154
hwanwoong0903 <ghksdnd02@gmail.com>
2026-04-09 13:33:04 +0900

    2026-04-09 퀴즈1번

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    int b;
+
+    cout << "이름을 입력하세요: ";
+    cin >> a;
+
+    cout << "학번을 입력하세요: ";
+    cin >> b;
+
+    cout << "이름: " << a << "\n";
+    cout << "학번: " << b << "\n";
+    
+    return 0;
+}
