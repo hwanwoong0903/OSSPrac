28c7129c8f9dd414e2c44bd05d09ace6f1fd4b46
hwanwoong0903 <ghksdnd02@gmail.com>
2026-04-09 14:24:11 +0900

    2026-04-09 퀴즈2 6번

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     string a, c;
-    int b;
+    int b, d;
 
     cout << "이름을 입력하세요: ";
     cin >> a;
@@ -16,8 +16,12 @@ int main()
     cout << "학과를 입력하세요: ";
     cin >> c;
 
+    cout << "학년를 입력하세요: ";
+    cin >> d;
+
     cout << "이름: " << a << "\n";
     cout << "학번: " << b << "\n";
     cout << "학과: " << c << "\n";
+    cout << "학년: " << d << "\n";
     return 0;
 }
