4e352aeb32439d2be8a125d21489edcddd1a1ea2
hwanwoong0903 <ghksdnd02@gmail.com>
2026-04-09 14:18:28 +0900

    2026-04-09 퀴즈2 4번

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    string a, c;
+    string a, c, d;
     int b;
 
     cout << "이름을 입력하세요: ";
@@ -15,9 +15,15 @@ int main()
 
     cout << "학과를 입력하세요: ";
     cin >> c;
+    
+    cout << "학교를 입력하세요: ";
+    cin >> d;
 
+	
     cout << "이름: " << a << "\n";
     cout << "학번: " << b << "\n";
     cout << "학과: " << c << "\n";
+    cout << "학교: " << d << "\n";
+    
     return 0;
 }
